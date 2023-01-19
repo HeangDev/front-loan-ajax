7f5843c3a288d6c37c8a05af816b011053af32f3
Kimheang-Developer <simkimheang4@gmail.com>
2023-01-19 10:33:28 +0700

    Heang Update

--- src/components/UserLoan.js
+++ src/components/UserLoan.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import { userloan } from '../data/userloan'
+import React, { useState, useEffect } from 'react'
+import http from '../services/api'
 
 const UserLoan = () => {
+    const [message, setMessage] = useState()
+
+    const fetchMessage = async () => {
+        await http.get(`api/message`).then(({data}) => {
+            setMessage(data)
+        })
+    }
+
+    useEffect(() => {
+        fetchMessage()
+    }, [])
     return (
         <>
             <div className="loan_wrap">
                 <marquee scrollamount="2" scrolldelay="80" direction="up">
-                    {userloan.map((item, i) => {
-                        return (
-                            <div className="loan" key={i}>
-                                <span className="loan_date">{item.date} </span>
-                                :
-                                <span className="loan_phone"> {item.tel} </span>
-                                กู้เงินสำเร็จ
-                                <span className="loan_money"> {item.amount}</span>฿!
-                            </div>    
+                    {
+                        message && message.length > 0 && (
+                            message.map((item, i) => {
+                                return (
+                                    <div className="loan" key={i}>
+                                        <span className="loan_date">{item.date} </span>
+                                        :
+                                        <span className="loan_phone"> {item.tel} </span>
+                                        กู้เงินสำเร็จ
+                                        <span className="loan_money"> {item.amount}</span>฿!
+                                    </div>
+                                )
+                            })
                         )
-                    })}
+                    }
                 </marquee>
             </div>
         </>
